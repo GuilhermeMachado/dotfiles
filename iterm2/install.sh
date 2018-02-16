e13b794040501259e17d26657ac4a0f437f328d8
GuilhermeMachado <guilherme.menon.machado@gmail.com>
2018-02-15 22:32:55 -0200

    fix .zshrc file and update install script

---
+++ iterm2/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# clone
+git clone https://github.com/powerline/fonts.git --depth=1
+
+# install fonts
+fonts./install.sh
+rm -rf fonts
+
+# copy .zshrc file
+echo Copying .zshrc backup file
+cp .zshrc ~/
+
+#install themes
+echo Installing themes
+cd /Users/guilhermemachado/.oh-my-zsh/themes/
+git clone https://github.com/agnoster/agnoster-zsh-theme.git
+mv agnoster-zsh-theme/agnoster.zsh-theme /Users/guilhermemachado/.oh-my-zsh/themes/
+rm -rf agnoster-zsh-theme
