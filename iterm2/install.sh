738e65e88c9d9131f6052887fc0ae65b57dd1dc8
Guilherme Machado <guilherme.menon.machado@gmail.com>
2022-08-21 21:03:06 -0300

    Update install.sh

--- iterm2/install.sh
+++ iterm2/install.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-# clone
+
+# Install oh-my-zsh https://ohmyz.sh/#install
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install fonts
 git clone https://github.com/powerline/fonts.git --depth=1
 
-# install fonts
-fonts./install.sh
-rm -rf fonts
+fonts/./install.sh
 
-# copy .zshrc file
+# Copy .zshrc file
 echo Copying .zshrc backup file
 cp .zshrc ~/
 
-#install themes
+# Install themes
 echo Installing themes
-cd /Users/gmachado/.oh-my-zsh/themes/
+cd  ~/.oh-my-zsh/themes/
 git clone https://github.com/agnoster/agnoster-zsh-theme.git
-mv agnoster-zsh-theme/agnoster.zsh-theme /Users/gmachado/.oh-my-zsh/themes/
-rm -rf agnoster-zsh-theme
+mv agnoster-zsh-theme/agnoster.zsh-theme ~/.oh-my-zsh/themes/
